a3346b5902b2901778b9bd013df54cf6407b2a72
Suranjan <suranjanbiswas2002@gmail.com>
2021-02-03 01:03:06 +0530

    Added the animations using the css animations and transitions

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# React Animations DEMO project
+
+## Using multiple ways for applying the react animations
+
+1. Used CSS Animations for the animations in the react app.
+2. Used the ReactTransitionGroup for the transitioning related animations.
+3. Used multiple higher level techniques for this demo project.
+
+## Used a simple to-do list for the implmentations
+
+You can see this project by cloning the repository and using npm-start in your cmd.
+
+Cheers!
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
+
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  openModalHandler = () => {
+    this.setState({ showModal: true });
+  };
+
+  closeModalHandler = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>React Animations</h1>
+        <Modal show={this.state.showModal} closed={this.closeModalHandler} />
+        <Backdrop show={this.state.showModal} />
+        <button className='Button' onClick={this.openModalHandler}>
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
