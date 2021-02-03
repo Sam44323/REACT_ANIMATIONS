0555fe0c68fdce1023cbff3ff771e85cffd83f7d
Suranjan <suranjanbiswas2002@gmail.com>
2021-02-04 02:02:52 +0530

    Completed the transition project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Transition from 'react-transition-group/Transition';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
 import Backdrop from './components/Backdrop/Backdrop';
 import List from './components/List/List';
+import { TransitionGroup } from 'react-transition-group';
 
 class App extends Component {
   state = {
@@ -23,14 +23,7 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>React Animations</h1>
-        <Transition
-          in={this.state.showModal}
-          timeout={300}
-          mountOnEnter
-          unmountOnExit
-        >
-          {(state) => <Modal show={state} closed={this.closeModalHandler} />}
-        </Transition>
+        <Modal show={this.state.showModal} closed={this.closeModalHandler} />
         {this.state.showModal ? <Backdrop show /> : null}
         <button className='Button' onClick={this.openModalHandler}>
           Open Modal
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,24 +1,36 @@
 import React from 'react';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './Modal.css';
 
-const modal = (props) => {
-  const classes = [
-    'Modal',
-    props.show === 'entering'
-      ? 'ModalOpen'
-      : props.show === 'exiting'
-      ? 'ModalClosed'
-      : null,
-  ];
+//custom object for timing in CSSTransition
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
   return (
-    <div className={classes.join(' ')}>
-      <h1>A Modal</h1>
-      <button className='Button' onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed',
+      }}
+    >
+      <div className='Modal'>
+        <h1>A Modal</h1>
+        <button className='Button' onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
